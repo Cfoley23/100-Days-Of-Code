2a7021fc4820007a22b0a94b95dd47302ab2e4d6
Cfoley23 <59401935+Cfoley23@users.noreply.github.com>
2021-01-07 23:43:45 -0600

    fixed bug in simple_calculator.py

--- Month 01/Week 01/Day 03/simple_calculator.py
+++ Month 01/Week 01/Day 03/simple_calculator.py
@@ -1,44 +1,38 @@
-from art import calc_art, calc_logo
-
-print(calc_logo)
-print(calc_art)
-
-#Addition
+#Calculator
 def add(n1, n2):
-    return n1, n2
+  return n1 + n2
 
-#Subtract
 def subtract(n1, n2):
-    return n1 - n2
+  return n1 - n2
 
-#Multiplication
 def multiply(n1, n2):
-    return n1 * n2
+  return n1 * n2
 
-#Division
 def divide(n1, n2):
-    n1 / n2
+  return n1 / n2
 
 operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide,
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
 }
 
-num1 =int(input("What's the first number?"))
-num2 =int(input("What's the second number?"))
+num1 = int(input("What's the first number?: "))
 for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation symbol from the lines above: ")
+  print(symbol)
+
+operation_symbol = input("Pick an operation: ")
+num2 = int(input("What's the next number?: "))
 calculation_function = operations[operation_symbol]
 first_answer = calculation_function(num1, num2)
 
 print(f"{num1} {operation_symbol} {num2} = {first_answer}")
 
-operation_symbol = input("pick another operation: ")
-num3 = int(input("What is the next number?: "))
+operation_symbol = input("Pick an operation: ")
+num3 = int(input("What's the next number?: "))
 calculation_function = operations[operation_symbol]
 second_answer = calculation_function(calculation_function(num1, num2), num3)
+second_answer = calculation_function(first_answer, num3)
 
 print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
