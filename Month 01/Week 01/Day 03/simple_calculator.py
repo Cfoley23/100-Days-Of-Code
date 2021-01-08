a86613322b167b489dfc97e4130e63f1e4f35155
Cfoley23 <59401935+Cfoley23@users.noreply.github.com>
2021-01-07 23:58:52 -0600

    refactoring code

--- Month 01/Week 01/Day 03/simple_calculator.py
+++ Month 01/Week 01/Day 03/simple_calculator.py
@@ -1,3 +1,6 @@
+from art import calc_logo, calc_art
+print(calc_art)
+print(calc_logo)
 #Calculator
 def add(n1, n2):
   return n1 + n2
@@ -21,18 +24,16 @@ operations = {
 num1 = int(input("What's the first number?: "))
 for symbol in operations:
   print(symbol)
+should_continue = True
+while should_continue:
+  operation_symbol = input("Pick an operation: ")
+  num2 = int(input("What's the next number?: "))
+  calculation_function = operations[operation_symbol]
+  answer = calculation_function(num1, num2)
 
-operation_symbol = input("Pick an operation: ")
-num2 = int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1, num2)
+  print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-operation_symbol = input("Pick an operation: ")
-num3 = int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(calculation_function(num1, num2), num3)
-second_answer = calculation_function(first_answer, num3)
-
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+  if input(f"Type 'y' to continue calculating with {answer}, or 'n' to exit: ")== "y":
+    num1 = answer
+  else:
+    should_continue = False
