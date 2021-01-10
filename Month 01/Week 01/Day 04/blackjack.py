388007c8bef3dbb3095284aeb2ba60d071fc9434
Cfoley23 <59401935+Cfoley23@users.noreply.github.com>
2021-01-09 22:04:58 -0600

    log entry

--- Month 01/Week 01/Day 04/blackjack.py
+++ Month 01/Week 01/Day 04/blackjack.py
@@ -8,6 +8,14 @@ def deal_card():
     card = random.choice(cards)
     return card
 
+def calculate_score(cards):
+    '''Take a list of cards and calculate a score from that list'''
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
 user_cards  = []
 computer_cards = []
 
@@ -15,12 +23,13 @@ for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-print(computer_cards)
-print(user_cards)
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f'    Your Cards: {user_cards}, current score: {user_score}')
+print(f' Computer's first card)
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
 
-def calculate_score(cards):
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
-    return sum(cards)
+
 
 
