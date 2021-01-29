accfcb94a17ed25b8da6ce0f92003c6b5d1bdec8
Cfoley23 <59401935+Cfoley23@users.noreply.github.com>
2021-01-28 23:58:27 -0600

    random walk.py .... modified a little.  :)

---
+++ Month 01/Week 04/Day 28/random walk.py
@@ -0,0 +1,46 @@
+import turtle as t
+import random
+
+screen = t.Screen()
+
+timmy = t.Turtle()
+timmy.shape('turtle')
+timmy.speed(0)
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
+def make_random_walk(step_number):
+    timmy.penup()
+
+    for _ in range(step_number):
+        screen.bgcolor(random_color())
+        timmy.color('olivedrab')
+        timmy.dot(random.randint(5, 60), random_color())
+        timmy.setheading(90 * random.randint(0, 3))
+        timmy.forward(random.randint(1, 50))
+        timmy.pensize(random.randint(1, 10))
+
+
+make_random_walk(500)
+
+# def draw_shapes(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         timmy.forward(100)
+#         timmy.right(angle)
+#
+#
+# for shape_side_n in range(3, 11):
+#     timmy.color(random.choice(colors))
+#     draw_shapes(shape_side_n)
+
+
+screen.exitonclick()
